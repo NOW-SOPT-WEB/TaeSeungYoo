e3ca548d95b9e9bc9012db085f9def9c951b19c1
taeseung-yoo <remicon99@gmail.com>
2024-05-08 23:48:10 +0900

    feat: path alias 설정

--- week4/assignment/auth-practice/src/App.tsx
+++ week4/assignment/auth-practice/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { Global } from '@emotion/react';
-
-import GlobalStyle from './styles/global';
+import GlobalStyle from '@styles/global';
 
 function App() {
   return (
     <>
       <Global styles={GlobalStyle} />
+      <h1>asdfasdf</h1>
+      <h1>asdfasdf</h1>
+      <h1>asdfasdf</h1>
+      <h1>asdfasdf</h1>
     </>
   );
 }
