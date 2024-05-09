a2c07b2b2b8c933b386460877fbc3bf3a4adea5b
taeseung-yoo <remicon99@gmail.com>
2024-05-10 02:11:20 +0900

    feat: 로그인 페이지 퍼블리싱

--- week4/assignment/auth-practice/src/pages/auth/login/index.tsx
+++ week4/assignment/auth-practice/src/pages/auth/login/index.tsx
@@ -1,7 +1,24 @@
+import loginImg from '@img/login-img.png';
+
 import { LoginContainer } from './styles';
 
 const index = () => {
-  return <LoginContainer>login</LoginContainer>;
+  return (
+    <LoginContainer>
+      <h1 className="login__title">로그인</h1>
+      <img className="login__img" src={loginImg} alt="login-logo" />
+      <section className="login__input">
+        <h3>ID</h3>
+        <input type="text" />
+        <h3>Password</h3>
+        <input type="password" />
+      </section>
+      <section className="login__button">
+        <button type="button">로그인</button>
+        <button type="button">회원가입</button>
+      </section>
+    </LoginContainer>
+  );
 };
 
 export default index;
--- week4/assignment/auth-practice/src/pages/auth/login/styles.ts
+++ week4/assignment/auth-practice/src/pages/auth/login/styles.ts
@@ -1,4 +1,53 @@
 import styled from '@emotion/styled';
 import { Generators, Colors } from '@styles';
 
-export const LoginContainer = styled.div``;
+export const LoginContainer = styled.div`
+  ${Generators.flexGenerator('column')}
+  margin: 5rem 0;
+
+  & > .login__title {
+    font-size: 3rem;
+    font-weight: 600;
+  }
+
+  & > .login__img {
+    width: 100px;
+    height: 100px;
+    margin-top: 2rem;
+  }
+
+  & > .login__input {
+    & > h3 {
+      font-size: 1.5rem;
+      margin-top: 2rem;
+    }
+
+    & > input {
+      width: 100%;
+      height: 2.5rem;
+      font-size: 1.5rem;
+      margin-top: 1rem;
+      padding: 0.5rem;
+    }
+  }
+
+  & > .login__button {
+    margin-top: 2rem;
+
+    & > button {
+      width: 100%;
+      height: 3rem;
+      font-size: 1.5rem;
+      margin-top: 1rem;
+      padding: 0.5rem;
+      background-color: ${Colors.Blue};
+      color: ${Colors.White};
+      border: none;
+      cursor: pointer;
+    }
+
+    & > button:last-of-type {
+      background-color: ${Colors.Gray};
+    }
+  }
+`;
