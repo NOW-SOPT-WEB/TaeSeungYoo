1e5dca5d1fb56a6088ce560789688881c5c15653
taeseung-yoo <remicon99@gmail.com>
2024-05-13 04:22:13 +0900

    refactor: 주석 추가

--- week4/assignment/auth-practice/src/hooks/useJoin.ts
+++ week4/assignment/auth-practice/src/hooks/useJoin.ts
@@ -6,39 +6,51 @@ import { useNavigate } from 'react-router-dom';
 const useJoin = () => {
   const navigate = useNavigate();
 
+  // input focus를 위한 ref
   const idRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const nicknameRef = useRef<HTMLInputElement>(null);
   const phoneNumberRef = useRef<HTMLInputElement>(null);
 
+  // id, password, nickname, phoneNumber를 관리하는 state
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const [nickname, setNickname] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
+  // id, password, nickname, phoneNumber 에러 여부를 관리하는 state
   const [idError, setIdError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const [nicknameError, setNicknameError] = useState(false);
   const [phoneNumberError, setPhoneNumberError] = useState(false);
 
+  // id input이 변경될 때 호출되는 함수
   const handleChangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
     setId(e.target.value);
   };
+  // password input이 변경될 때 호출되는 함수
   const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
+  // nickname input이 변경될 때 호출되는 함수
   const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
   };
+  // phoneNumber input이 변경될 때 호출되는 함수
   const handleChangePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhoneNumber(formatPhoneNumber(e.target.value));
   };
 
+  // 회원가입 버튼 클릭 시 호출되는 함수
+  // id, password, nickname, phoneNumber가 유효한지 검사하는 함수
   const validateJoin = () => {
+    // id, password, nickname, phoneNumber 에러 여부 초기화
     setIdError(false);
     setPasswordError(false);
     setNicknameError(false);
     setPhoneNumberError(false);
+
+    // id가 입력되지 않았을 경우 alert 출력 후 focus
     if (!id) {
       alert('아이디를 입력해주세요.');
       if (idRef.current) {
@@ -48,6 +60,7 @@ const useJoin = () => {
       return false;
     }
 
+    // password가 입력되지 않았을 경우 alert 출력 후 focus
     if (!password) {
       alert('비밀번호를 입력해주세요.');
       if (passwordRef.current) {
@@ -57,6 +70,7 @@ const useJoin = () => {
       return false;
     }
 
+    // password 형식이 맞지 않을 경우 alert 출력 후 focus
     if (!validatePassword(password)) {
       alert('비밀번호 형식이 맞지 않습니다.');
       if (passwordRef.current) {
@@ -66,6 +80,7 @@ const useJoin = () => {
       return false;
     }
 
+    // nickname이 입력되지 않았을 경우 alert 출력 후 focus
     if (!nickname) {
       alert('닉네임을 입력해주세요.');
       if (nicknameRef.current) {
@@ -75,6 +90,7 @@ const useJoin = () => {
       return false;
     }
 
+    // phoneNumber가 입력되지 않았을 경우 alert 출력 후 focus
     if (!phoneNumber) {
       alert('전화번호를 입력해주세요.');
       if (phoneNumberRef.current) {
@@ -87,7 +103,9 @@ const useJoin = () => {
     return true;
   };
 
+  // 회원가입 버튼 클릭 시 호출되는 함수
   const handleClickJoin = async () => {
+    // id, password, nickname, phoneNumber가 유효하지 않을 경우 return
     if (!validateJoin()) return;
     const response = await postJoin({ authenticationId: id, password, nickname, phone: phoneNumber });
     const { code, message } = response && response.data;
@@ -100,6 +118,8 @@ const useJoin = () => {
     }
   };
 
+  // 뒤로가기 버튼 클릭 시 호출되는 함수
+  // login에도 있고 join에도 있는 함수라서 공통으로 빼는게 좋을 듯
   const handleClickBack = () => {
     navigate(-1);
   };
--- week4/assignment/auth-practice/src/hooks/useLogin.ts
+++ week4/assignment/auth-practice/src/hooks/useLogin.ts
@@ -5,21 +5,32 @@ import { useNavigate } from 'react-router-dom';
 const useLogin = () => {
   const navigate = useNavigate();
 
+  // id를 관리하는 state
   const [id, setId] = useState('');
+  // id 에러 메시지를 관리하는 state
   const [idInfoText, setIdInfoText] = useState('');
+  // password를 관리하는 state
   const [password, setPassword] = useState('');
+  // password 에러 메시지를 관리하는 state
   const [passwordInfoText, setPasswordInfoText] = useState('');
 
+  // id input이 변경될 때 호출되는 함수
   const handleChangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
     setId(e.target.value);
   };
+
+  // password input이 변경될 때 호출되는 함수
   const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
 
+  // 로그인 버튼 클릭 시 호출되는 함수
   const handleClickLogin = async () => {
+    // id, password 에러 메시지 초기화
     setIdInfoText('');
     setPasswordInfoText('');
+
+    // id, password가 입력되지 않았을 경우 에러 메시지 설정 후 return
     if (!id) {
       setIdInfoText('아이디를 입력해주세요.');
       return;
@@ -28,19 +39,24 @@ const useLogin = () => {
       setPasswordInfoText('비밀번호를 입력해주세요.');
       return;
     }
+
+    // id, password를 서버로 전송하여 로그인 요청
     const response = await postLogin({ authenticationId: id, password });
     const { code, message } = response && response.data;
 
     if (code === 200) {
       const memberId = response && response.headers.location;
 
+      // 로그인 성공 시 memberId를 path로 설정하고 alert로 '로그인 성공' 메시지 출력
       alert('로그인 성공');
       navigate(`/${memberId}`);
     } else {
+      // 로그인 실패 시 alert로 message 출력
       alert(message);
     }
   };
 
+  // 뒤로가기 버튼 클릭 시 호출되는 함수
   const handleClickBack = () => {
     navigate(-1);
   };
